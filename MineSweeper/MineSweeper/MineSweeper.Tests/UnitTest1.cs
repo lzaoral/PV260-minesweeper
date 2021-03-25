1f162261cd2806f7b41295bf3c0ba461cbdadbd5
zvon <tom.zvon@gmail.com>
2021-03-25 14:57:46 +0100

    Initial Solution

---
+++ MineSweeper/MineSweeper/MineSweeper.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace MineSweeper.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
